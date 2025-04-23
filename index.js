8117bb8a9605067c7bfe5f0d749a2d201debfede
beingharisali <theharis99@gmail.com>
2025-04-24 00:07:07 +0500

    login and sign up functionality done

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
+// require("express-async-errors");
 const express = require("express");
 const app = express();
 
 // extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
+// const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 // connect db
 const connectDB = require("./database/connect");
+
+// routers
+const authRouter = require("./routes/auth");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -25,12 +29,15 @@ app.use(
 );
 app.use(helmet());
 app.use(cors());
-app.use(xss());
+// app.use(xss());
+
+// routes
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const start = async () => {
   try {
